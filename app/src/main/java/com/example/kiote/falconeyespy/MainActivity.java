18f5f0290a0e784bfbbed080221a3430219463fd
Ekaterina Krivich <krivich.ekaterina@gmail.com>
2015-05-18 13:52:17 +0300

    does nothing but initialize camera object

--- app/src/main/java/com/example/kiote/falconeyespy/MainActivity.java
+++ app/src/main/java/com/example/kiote/falconeyespy/MainActivity.java
@@ -1,51 +1,36 @@
 package com.example.kiote.falconeyespy;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
+import android.graphics.PixelFormat;
+import android.hardware.Camera;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.EditText;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 
 
-public class MainActivity extends ActionBarActivity {
-    public final static String EXTRA_MESSAGE = "com.mycompany.myfirstapp.MESSAGE";
+public class MainActivity extends AppCompatActivity {
+    SurfaceView surfaceView;
+    SurfaceHolder surfaceHolder;
+
+    public static Camera getCameraInstance(){
+        Camera mCamera = null;
+        try {
+            mCamera = Camera.open();
+        } catch (RuntimeException e) {
+            Log.e("Camera", "failed with " + e.getMessage());
+        }
+        return mCamera;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
 
-    /** Called when the user clicks the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.edit_message);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+        Camera mCamera = getCameraInstance();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
+        mCamera.release();
+        Log.i("Camera", "Done");
     }
 }
